a6c77aeb33550c3812efdebff0c07e916b26744c
JayntiRaj <jayntiraj77@gmail.com>
2019-11-14 00:40:01 -0800

    Created backgrounded

--- Saffire Game/GameViewController.swift
+++ Saffire Game/GameViewController.swift
@@ -17,14 +17,16 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            let scene = GameScene(size: view.bounds.size)
-            // Set the scale mode to scale to fit the window
-            scene.scaleMode = .aspectFill
-            
-            // Present the scene
-            view.presentScene(scene)
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
             
             view.ignoresSiblingOrder = true
+            
             view.showsFPS = true
             view.showsNodeCount = true
         }
